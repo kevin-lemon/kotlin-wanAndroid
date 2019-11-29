c87644312d3b3375b64f0776db13d1c5e4e5e95f
wangxukai <765002108@qq.com>
2019-11-29 13:38:46 +0800

    init commit

---
+++ app/src/main/java/com/lemon/wanandroid/BaseActivity.kt
@@ -0,0 +1,28 @@
+package com.lemon.wanandroid
+
+import android.content.Context
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by wxk on 2019/11/29.
+ */
+abstract class BaseActivity : AppCompatActivity(){
+    protected lateinit var mContext: Context
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(getContentViewId())
+        mContext = this
+        initView()
+    }
+
+    abstract fun getContentViewId():Int
+    abstract fun initView()
+    abstract fun initData()
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
---
+++ app/src/main/java/com/lemon/wanandroid/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.lemon.wanandroid
+
+/**
+ * Created by wxk on 2019/11/29.
+ */
+ class SplashActivity : BaseActivity(){
+    override fun getContentViewId(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun initView() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun initData() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
