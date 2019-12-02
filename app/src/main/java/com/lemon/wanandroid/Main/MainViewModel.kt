5e712537e6a3908980f7ef62ea59cb0ba4f60cdb
wangxukai <765002108@qq.com>
2019-12-02 19:09:04 +0800

    retrofit引用

--- app/src/main/java/com/lemon/wanandroid/Main/MainViewModel.kt
+++ app/src/main/java/com/lemon/wanandroid/Main/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.lemon.wanandroid.Main
 
+import android.app.Application
 import androidx.lifecycle.*
 import com.lemon.wanandroid.Repository.UserRepository
 import com.lemon.wanandroid.bean.User
@@ -7,7 +8,7 @@ import com.lemon.wanandroid.bean.User
 /**
  * Created by Lemon on 2019/11/29.
  */
-class MainViewModel constructor(savedStateHandle: SavedStateHandle,userRepository: UserRepository):ViewModel(){
+open class MainViewModel constructor(savedStateHandle: SavedStateHandle): ViewModel() {
     val userId: String = savedStateHandle["uid"]?:throw IllegalArgumentException("missing user id")
-    val user : LiveData<User> = userRepository.getUser()
+    val user : LiveData<User> = UserRepository().getUser()
 }
