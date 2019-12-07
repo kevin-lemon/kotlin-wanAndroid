b1e2d8825eb7340e87490faa3587e0ec2f441d6e
kevin <765002108@qq.com>
2019-12-07 19:12:24 +0800

    请求接口

--- app/src/main/java/com/lemon/wanandroid/utils/LiveDataCallAdapterFactory.kt
+++ app/src/main/java/com/lemon/wanandroid/utils/LiveDataCallAdapterFactory.kt
@@ -24,7 +24,6 @@ class LiveDataCallAdapterFactory : CallAdapter.Factory() {
         if (observableType !is ParameterizedType) {
             throw IllegalArgumentException("resource must be parameterized")
         }
-        val bodyType = CallAdapter.Factory.getParameterUpperBound(0, observableType)
-        return LiveDataCallAdapter<Any>(bodyType)
+        return LiveDataCallAdapter<Any>(observableType)
     }
 }
